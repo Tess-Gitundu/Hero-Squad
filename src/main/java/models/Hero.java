c8049be13efdb6a5aa67a1a2b0538627765d7821
Tess <njerigitundu003@gmail.com>
2022-05-03 15:27:10 +0300

    Generate getters and setters

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -13,5 +13,35 @@ public class Hero {
         this.weakness = weakness;
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSpecialPower() {
+        return specialPower;
+    }
+
+    public void setSpecialPower(String specialPower) {
+        this.specialPower = specialPower;
+    }
+
+    public String getWeakness() {
+        return weakness;
+    }
+
+    public void setWeakness(String weakness) {
+        this.weakness = weakness;
+    }
 }
